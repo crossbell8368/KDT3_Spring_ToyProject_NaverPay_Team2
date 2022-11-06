f24308ad561d0b58f23db69ecb1a9bf1375ef505
Yuki <skool109@gmail.com>
2022-11-07 01:04:15 +0900

    Style: Code Formatting

--- controller/src/main/java/org/example/overview/members/controller/nonlogin/LoginController.java
+++ controller/src/main/java/org/example/overview/members/controller/nonlogin/LoginController.java
@@ -8,21 +8,21 @@ import org.example.overview.utils.Status;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-// 20221107 생성한 url 수정된 버전입니다.
 @Controller
 @RequestMapping("/")
 public class LoginController {
 
     private SessionMgr sessionMgr;
     private CookieMgr cookieMgr;
-    private MemberService memberService ;
-
+    private MemberService memberService;
 
     @Autowired
     public LoginController(SessionMgr sessionMgr, CookieMgr cookieMgr, MemberService memberService) {
@@ -33,9 +33,9 @@ public class LoginController {
 
     @GetMapping("/login")
     public String loginPage(HttpServletRequest request, HttpSession session) {
-        String view = "login";
+        String view = "/members/nonlogin/login";
 
-        if (session.getAttribute("SESSION_ID") != null) { // 로그인이 되어있는 상태
+        if (session.getAttribute("SESSION_ID") != null) {
             return "redirect:/";
         }
 
@@ -43,8 +43,8 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String doLogin(@RequestParam String uId, @RequestParam String uPw, @RequestParam(required = false) String save,
-                          Model model, HttpServletRequest request, HttpSession session, HttpServletResponse response) {
+    public String doLogin(@RequestParam String uId, @RequestParam String uPw, Model model,
+                          HttpServletRequest request, HttpSession session) {
 
         String view = loginPage(request, session);
         Status respStatus = Status.FAIL;
@@ -53,19 +53,13 @@ public class LoginController {
         if (memberDTO != null) {
             sessionMgr.create(session, uId);
 
-
             view = "redirect:/";
             respStatus = Status.SUCCESS;
         }
-
 
         session.setAttribute("login", respStatus);
         model.addAttribute("uId", session.getAttribute("SESSION_ID"));
 
         return view;
-
     }
-
 }
-
-
