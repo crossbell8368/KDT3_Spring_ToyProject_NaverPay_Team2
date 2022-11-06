a226c80b35b3846fed0497aeb155c900d96c9c91
dkschdcks <113900719+dkschdcks@users.noreply.github.com>
2022-11-06 23:51:22 +0900

    Feat: Add LoginController

--- controller/src/main/java/org/example/overview/members/controller/login/LogoutController.java
+++ controller/src/main/java/org/example/overview/members/controller/login/LogoutController.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @Controller
-@RequestMapping("/members")
+@RequestMapping("/login")
 public class LogoutController { // 로그아웃 컨트롤러
     private MemberService memberService; // = MemberService.getInstance();
 
