0a513562271eedc33769dedc59dfe85f95403f3b
mystagogy <108065594+mystagogy@users.noreply.github.com>
2022-11-08 16:02:23 +0900

    Revert "feat: Create Payment RestController, Service etc."

--- controller/src/main/java/org/example/overview/members/dao/IPaymentDAO.java
+++
@@ -1,8 +0,0 @@
-package org.example.overview.members.DAO;
-
-import org.example.overview.members.Entity.Payment;
-
-public interface IPaymentDAO {
-    Payment select(String oId, String uId);
-    int delete(String id);
-}
