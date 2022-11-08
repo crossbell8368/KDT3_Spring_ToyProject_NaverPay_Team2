ae159c04bc84e7a822b32e72b88b7462a8e9e406
crossbell8368 <godfather346@gmail.com>
2022-11-08 16:37:45 +0900

    Revert "Merge pull request #19 from crossbell8368/prod_payment"
    This reverts commit 719eb16cc55bafc8517e768f33cdbefb4e6c94f9, reversing
    changes made to 4fc62e20cb07ac8ec3559dbda65e69e0a63fabf7.

--- controller/src/main/java/org/example/overview/members/service/IPaymentService.java
+++
@@ -1,9 +0,0 @@
-package org.example.overview.members.service;
-
-import org.example.overview.members.DTO.PaymentDTO;
-
-public interface IPaymentService {
-    boolean save(String uId, String title, String completed);
-    PaymentDTO getPaymentById(String oId, String uId);
-    boolean deletePaymentById(String oId, String uId);
-}
